e8a12e44d8aa6bec1e77b13b15bf4d07a8c2f3a9
Hadia <hadiaandar@gmail.com>
2019-02-19 18:35:56 -0800

    Added working twitter app

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Hadia Andar on 2/19/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
